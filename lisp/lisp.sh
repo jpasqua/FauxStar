c431351bfe6cf189a9f68a67b61a1420533242f8
Joe Pasqua <joe@noroomattheinn.org>
2024-09-23 20:16:27 -0700

    Improve the lisp launch script

--- lisp/lisp.sh
+++ lisp/lisp.sh
@@ -4,9 +4,9 @@ ADDITIONAL_PARAMS=""
 
 
 usage() {
-    echo "Usage: $0 [--fullscreen] [emulator_parameters...]"
+    echo "Usage: $0 [-?]|[-fullscreen] [emulator_parameters...]"
     echo "Example:"
-    echo "  $0 --fullscreen"
+    echo "  $0 -fullscreen"
     echo
     echo "For a guided process, provide no parameters"
     echo "  $0"
@@ -14,14 +14,18 @@ usage() {
     exit 1
 }
 
+do_fullscreen() {
+    export VNCVIEWER_FULLSCREEN="-FullScreen"
+    dims=$(xrandr 2>/dev/null | grep '*' | awk '{print $1}')
+    ADDITIONAL_PARAMS="$ADDITIONAL_PARAMS -n -s ${dims} -v +"
+}
+
 choose_options() {
 	# VNCVIEWER_FULLSCREEN is used by our custom medley_vnc.sh script
 	export VNCVIEWER_FULLSCREEN=""
     read -p "Fullscreen mode? ([y]/n): " fullscreen_choice
     if [[ "$fullscreen_choice" == "y" || "$fullscreen_choice" == "Y" || "$fullscreen_choice" == "" ]]; then
-        export VNCVIEWER_FULLSCREEN="-FullScreen"
-        dims=$(xrandr 2>/dev/null | grep '*' | awk '{print $1}')
-        ADDITIONAL_PARAMS="$ADDITIONAL_PARAMS -n -s ${dims} -v +"
+        do_fullscreen
     fi
 }
 
@@ -33,7 +37,13 @@ BASE_DIR=$(realpath "$(dirname "$0")")
 if [ $# -eq 0 ]; then
     choose_options
 elif [ $# -eq 1 ]; then
-	usage
+    if [ $1 == "-?" ]; then
+    	usage
+    fi
+    if [[ "${1,,}" == "-fullscreen" ]]; then
+        shift
+        do_fullscreen
+    fi
 fi
 
-echo medley $ADDITIONAL_PARAMS $@
+medley $ADDITIONAL_PARAMS $@
