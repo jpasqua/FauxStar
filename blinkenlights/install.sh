88215673ee12957807520ac708179ed4fd78658f
Joe Pasqua <joe@noroomattheinn.org>
2024-10-23 17:49:21 -0700

    Formatting of output

--- blinkenlights/install.sh
+++ blinkenlights/install.sh
@@ -4,11 +4,21 @@ echo "Installing the blinkenlights functionality"
 
 pushd blinkenlights
 
+echo "== Creating a python virtual environment"
 python3 -m venv bl-env
 source bl-env/bin/activate
+echo "== Installing libraries..."
+echo ""
+echo "   == adafruit-circuitpython-ssd1306"
 pip install adafruit-circuitpython-ssd1306
+echo ""
+echo "   == adafruit-circuitpython-busdevice"
 pip install adafruit-circuitpython-busdevice
+echo ""
+echo "   == pillow"
 pip install pillow
+echo ""
+echo "   == adafruit-blinka"
 pip install adafruit-blinka
 deactivate
 
